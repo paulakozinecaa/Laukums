b46162e7babf0816fff53cf793e2f9995953b108
paulakozinecaa <p.kozineca@gmail.com>
2022-02-28 15:19:46 +0200

    daleji strada

--- Index.html/script.js
+++ Index.html/script.js
@@ -36,5 +36,42 @@ function laukums(m1, m2, m3) {
     let tlaukums = Math.sqrt(
         pusper * ((pusper - m1) * (pusper - m2) * (pusper - m3)));
     console.log(tlaukums);
-    return laukums
+    return tlaukums
+}
+function irTrijsturis(m1, m2, m3) {
+    if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function rezultats() {
+    const m = nolasa();
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
+    console.log(m1, m2, m3);
+    if (!nolasa()) {
+        t = "Nekorekti ievaddati, trijstūra malu vērtībām ir jābūt lielākiem par 0";
+    } else {
+        if (irTrijsturis(m1, m2, m3) == false) {
+            t = "Trijstūris neesksistē, jo jebkuru divu mallu sumām ir jābūt lielākām par trešo malu";
+        } else {
+            t = "Trijstūris ar malu garumiem " + m1 + "," + m2 + "un " + m3 + "eksistē."
+            if (m1 == m2 && m2 == m3) {
+                t += "Tas ir vienādalu trijstūris"
+            }
+        }
+    }
+    const p = (m1, m2, m3);
+    const s = Math.round(laukums(m1, m2, m3) * 100) / 100;
+    t += "Perimetrs ir" + p + "un laukums ir" + s + ".";
+    console.log(t);
+    return t;
+}
+function izvadaTekstu(){
+    const teksts = rezultats();
+    console.log(teksts);
+    const sakne=document.getElementById("izvade");
+    const raksti = document.createElement("p");
+    sakne,innerHTML = teksts;
+    sakne.appendChild(raksti);
 }
